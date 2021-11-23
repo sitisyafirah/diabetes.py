9a01cf5b92d54468a542337d9db428eee412b5b6
sitisyafirah <94594600+sitisyafirah@users.noreply.github.com>
2021-11-23 19:24:26 +0800

    Update diabetesapp.py

--- diabetesapp.py
+++ diabetesapp.py
@@ -8,33 +8,34 @@ classifier = pickle.load(pickle_in)
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
-st.title(" ~Welcome to Diabetes's Info~")
-st.write("Diabetes is a disease in which your blood glucose (blood sugar) levels are too high.     Glucose comes from food and insulin is a hormone that is created     in the body to help glucose get into cells to give them energy.")
-st.write("DIABETES AWARENESS BECAUSE IT MATTERS ")
-vid=open("example.mp4","rb")
-st.video(vid)
-st.markdown("<span style=“background-color:#121922”>",unsafe_allow_html=True)
-st.write("## Diabetes Rates by Country 2021")
-st.write("Please input country's name: (Example:India)")
-c=st.text_input(" ")
-sv=pd.read_csv('csvData.csv')
-d=sv[sv['Country'] == c]
-st.write("The number of diabeter's population is: ",d)
+if show:
+    st.title(" ~Welcome to Diabetes's Info~")
+    st.write("Diabetes is a disease in which your blood glucose (blood sugar) levels are too high.     Glucose comes from food and insulin is a hormone that is created     in the body to help glucose get into cells to give them energy.")
+    st.write("DIABETES AWARENESS BECAUSE IT MATTERS ")
+    vid=open("example.mp4","rb")
+    st.video(vid)
+    st.markdown("<span style=“background-color:#121922”>",unsafe_allow_html=True)
+    st.write("## Diabetes Rates by Country 2021")
+    st.write("Please input country's name: (Example:India)")
+    c=st.text_input(" ")
+    sv=pd.read_csv('csvData.csv')
+    d=sv[sv['Country'] == c]
+    st.write("The number of diabeter's population is: ",d)
 
-option = st.selectbox(
-    'Select Diabetes Rates by Country 2021 ', 
-    ('Top 10','Last 10'))
+    option = st.selectbox(
+        'Select Diabetes Rates by Country 2021 ', 
+        ('Top 10','Last 10'))
 
-if option=='Top 10':
-    result1 = sv.head(10)
-    st.write('Top 10 Diabetes Rates: ',result1)
-elif option=='Last 10':
-    result2 = sv.tail(10)
-    st.write('Last 10 Diabetes Rates: ',result2)
-else:
-    st.write("Please select")
+    if option=='Top 10':
+        result1 = sv.head(10)
+        st.write('Top 10 Diabetes Rates: ',result1)
+    elif option=='Last 10':
+        result2 = sv.tail(10)
+        st.write('Last 10 Diabetes Rates: ',result2)
+    else:
+        st.write("Please select")
 
-st.write("Now, lets fill the diabetes prediction form.(Please untick hide button). After fill the form. Please click predict to get the result")
+st.write("Now, lets fill the diabetes prediction form.(Please untick hide button). After fill the form, please click Predict to get the result")
 
 st.sidebar.header('Diabetes Prediction')
 select = st.sidebar.selectbox('Select Form', ['Form 1'], key='1')
